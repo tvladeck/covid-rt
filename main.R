5c75696ecd1c616f83d9d39f578094efe130cad0
Thomas Vladeck <tvladeck@ip-172-31-28-191.ec2.internal>
2020-04-26 17:05:08 +0000

    Confirming that I can write a function in the smooth direction in R

--- main.R
+++ main.R
@@ -36,6 +36,67 @@ found_onsets =
 found_cases = (onset_to_cases %*% (found_onsets$par[1:54] * cum_p_observed)) 
 ts.plot(cbind(found_cases, obs_cases, found_onsets$par[1:54]), col = c("red", "blue", "green"))
 
+
+ll_thetasteps_smoothdir = function(initial_onsetload, tau, theta_steps) {
+  thetas = cumsum(theta_steps)
+  log_delta_onset = rep(0, stan_timesteps)
+  log_delta_onset[1] = log(initial_onsetload)
+  log_delta_onset[2:stan_timesteps] = thetas
+  onsets = exp(cumsum(log_delta_onset)) 
+  observed_onsets = onsets * cum_p_observed
+  cases = onset_to_cases %*% observed_onsets
+  ll_thetasteps = sum(dnorm(theta_steps, mean = 0, sd = tau[1], log = T))
+  ll_cases = sum(dnbinom(obs_cases, mu = cases, size = tau[2], log = T))
+  ll = ll_thetasteps + ll_cases
+  return(ll)
+}
+
+find_onsets_smoothdir = function(par) {
+  initial_onsetload = par[1]
+  tau = rep(0, 2)
+  tau[1] = par[2]
+  tau[2] = par[3]
+  theta_steps = par[4:56]
+  ll_thetasteps_smoothdir(initial_onsetload, tau, theta_steps)
+}
+
+
+
+
+found_onsets = 
+  optim(c(rep(1, 3), rep(0, 53)), find_onsets_smoothdir, method = "L-BFGS-B", 
+        lower = c(rep(0.01, 3), rep(-Inf, 53)),
+        control = list(fnscale = -1, parscale = rep(1, 56)))
+
+par = found_onsets$par
+
+implied_onsets = function(par) {
+  initial_onsetload = par[1]
+  tau = rep(0, 2)
+  tau[1] = par[2]
+  tau[2] = par[3]
+  theta_steps = par[4:56]
+  
+  thetas = cumsum(theta_steps)
+  log_delta_onset = rep(0, stan_timesteps)
+  log_delta_onset[1] = log(initial_onsetload)
+  log_delta_onset[2:stan_timesteps] = thetas
+  onsets = exp(cumsum(log_delta_onset)) 
+  observed_onsets = onsets * cum_p_observed
+  cases = onset_to_cases %*% observed_onsets
+  return(
+    list(
+      onsets = onsets,
+      observed_onsets = observed_onsets,
+      cases = cases
+    )
+  )
+}
+
+
+
+
+
 modeled_state = c("massachusetts", "michigan")
 col_idx = which(colnames(dat_diff) == modeled_state)
 
